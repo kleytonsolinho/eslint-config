c4e14376c9eea6171a7403163662acfc4c18168e
Kleyton Solinho <kleytonsolinho@gmail.com>
2023-05-25 21:55:52 -0300

    fix: resolved imports + readme update

--- prettier.js
+++ prettier.js
@@ -1,10 +1,9 @@
 module.exports = {
-	singleQuote: true,
-	arrowParens: "always",
-	trailingComma: "es5",
-	useTabs: true,
 	printWidth: 80,
 	tabWidth: 2,
+	singleQuote: true,
+	trailingComma: "all",
+	arrowParens: "always",
 	semi: false,
 	endOfLine: "auto",
 }
